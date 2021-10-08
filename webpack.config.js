9026e9bb4055070d544f09151d326768ddf28e38
ahmed-Bazzara <ahmed.bazzara@tretton37.com>
2021-10-08 18:44:08 +0200

    use own webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,65 @@
 const path = require('path');
-const AwsSamPlugin = require('aws-sam-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
-const awsSamPlugin = new AwsSamPlugin({
-  projects: {
-    createList: './src/handlers/create-list-function',
-    getLists: './src/handlers/get-lists-function',
-  },
-  vscodeDebug: true,
-});
+// const AwsSamPlugin = require('aws-sam-webpack-plugin');
+
+// const awsSamPlugin = new AwsSamPlugin({
+//   projects: {
+//     createList: './src/handlers/create-list-function',
+//     getLists: './src/handlers/get-lists-function',
+//   },
+//   vscodeDebug: true,
+// });
 
 module.exports = {
   // Loads the entry object from the AWS::Serverless::Function resources in your
   // SAM config. Setting this to a function will
-  entry: () => awsSamPlugin.entry(),
+  // entry: () => awsSamPlugin.entry(),
 
-  // Write the output to the .aws-sam/build folder
-  output: {
-    filename: (chunkData) => awsSamPlugin.filename(chunkData),
-    libraryTarget: 'commonjs2',
-    path: path.resolve('.'),
+  entry: {
+    createFunction: {
+      import: path.resolve(
+        __dirname,
+        'src/handlers/create-list-function/create-list.ts'
+      ),
+
+      filename: 'createFunction/createFunction.js',
+    },
+    getFunction: {
+      import: path.resolve(
+        __dirname,
+        'src/handlers/get-lists-function/get-lists.ts'
+      ),
+      filename: 'getFunction/getFunction.js',
+    },
   },
+
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(
+            __dirname,
+            'src/handlers/create-list-function/template.yml'
+          ),
+          to: 'createFunction',
+        },
+        {
+          from: path.resolve(
+            __dirname,
+            'src/handlers/get-lists-function/template.yml'
+          ),
+          to: 'getFunction',
+        },
+      ],
+    }),
+  ],
+  // Write the output to the .aws-sam/build folder
+  // output: {
+  //   filename: (chunkData) => awsSamPlugin.filename(chunkData),
+  //   libraryTarget: 'commonjs2',
+  //   path: path.resolve(__dirname, 'dist'),
+  // },
 
   // Create source maps
   devtool: 'source-map',
@@ -38,7 +78,7 @@ module.exports = {
   externals: process.env.NODE_ENV === 'development' ? [] : ['aws-sdk'],
 
   // Set the webpack mode
-  mode: process.env.NODE_ENV || 'production',
+  mode: 'production',
 
   // Add the TypeScript loader
   module: {
@@ -46,5 +86,5 @@ module.exports = {
   },
 
   // Add the AWS SAM Webpack plugin
-  plugins: [awsSamPlugin],
+  // plugins: [awsSamPlugin],
 };
