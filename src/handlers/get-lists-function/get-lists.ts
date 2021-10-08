6f4b788223fe8c68d2f64aee3ca4b12291867819
ahmed-Bazzara <ahmed.bazzara@tretton37.com>
2021-10-08 16:27:51 +0200

    test building with webpack

--- src/handlers/get-lists-function/get-lists.ts
+++ src/handlers/get-lists-function/get-lists.ts
@@ -1,11 +1,12 @@
 
 import * as AWS from 'aws-sdk';
-import { buildResponse, scanDynamoRecords } from '../utilities';
-import { STOCKHOLM_REGION} from '../aws_constants';
+import { buildResponse, scanDynamoRecords } from '../../utilities';
+import { STOCKHOLM_REGION} from '../../aws_constants';
 AWS.config.update({ region: STOCKHOLM_REGION });
 
 const TableName = process.env.TABLE_NAME as string;
 export const getLists = async () => {
+  console.log('fetching list');
   const params = { TableName }
   const allCollections = await scanDynamoRecords(params, []);
   const body = {
