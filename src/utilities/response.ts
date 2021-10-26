ab728ab2eee60de5855e156362118e39c6e695dd
ahmed-Bazzara <ahmed.bazzara@tretton37.com>
2021-10-26 21:56:50 +0200

    Access-Control-Allow-Headers

--- src/utilities/response.ts
+++ src/utilities/response.ts
@@ -8,6 +8,7 @@ export const buildResponse = (statusCode: number, body?: any) => {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': process.env.NODE_ENV === 'development' ? "http://localhost:3001" : "https://www.letsdo-list.com",
       "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization, Origin"
     },
     body: JSON.stringify(body)
   }
