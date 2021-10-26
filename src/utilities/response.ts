4a474b7b21361828bfc2636f36620010d22c04b8
ahmed-Bazzara <ahmed.bazzara@tretton37.com>
2021-10-26 21:42:35 +0200

    remove comment

--- src/utilities/response.ts
+++ src/utilities/response.ts
@@ -6,7 +6,6 @@ export const buildResponse = (statusCode: number, body?: any) => {
     statusCode,
     headers: {
       'Content-Type': 'application/json',
-      // 'Access-Control-Allow-Origin': "http://localhost:3001",
       'Access-Control-Allow-Origin': process.env.NODE_ENV === 'development' ? "http://localhost:3001" : "https://www.letsdo-list.com",
       "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
     },
