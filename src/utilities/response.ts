5f85fe518403ddb44d4eab5644c1a7bb8814a7a7
ahmed-Bazzara <ahmed.bazzara@tretton37.com>
2021-10-26 21:19:24 +0200

    edit CORS config for the new domain

--- src/utilities/response.ts
+++ src/utilities/response.ts
@@ -6,8 +6,8 @@ export const buildResponse = (statusCode: number, body?: any) => {
     statusCode,
     headers: {
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': "http://localhost:3001",
-      // 'Access-Control-Allow-Origin': process.env.NODE_ENV === 'development' ? "http://localhost:3001" : "http://todo-client-bucket.s3-website.eu-north-1.amazonaws.com/",
+      // 'Access-Control-Allow-Origin': "http://localhost:3001",
+      'Access-Control-Allow-Origin': process.env.NODE_ENV === 'development' ? "http://localhost:3001" : "https://www.letsdo-list.com",
       "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
     },
     body: JSON.stringify(body)
