32c5ce0f88358dff82cddad9a353aee16ef770fd
ahmed-Bazzara <ahmed.bazzara@tretton37.com>
2022-02-11 17:43:57 +0100

    change cors for template prod

--- src/handlers/get-lists-function/get-lists.ts
+++ src/handlers/get-lists-function/get-lists.ts
@@ -1,21 +1,14 @@
-
 import * as AWS from 'aws-sdk';
 import { buildResponse, scanDynamoRecords } from '../../utilities';
-import { STOCKHOLM_REGION} from '../../aws_constants';
+import { STOCKHOLM_REGION } from '../../aws_constants';
 AWS.config.update({ region: STOCKHOLM_REGION });
 
 const TableName = process.env.TABLE_NAME as string;
 export const getLists = async () => {
-  console.log('fetching list');
-  console.log('TableName', TableName);
-  const params = { TableName }
+  const params = { TableName };
   const allCollections = await scanDynamoRecords(params, []);
-  console.log('allCollections', allCollections);
 
-  const body = allCollections
-  console.log(buildResponse(200, body));
-  console.log('end');
-  
-  
+  const body = allCollections;
+
   return buildResponse(200, body);
-}
+};
